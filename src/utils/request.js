7cefaf23f1db88982565298da287052d63ef77cd
mxm321 <15863722017@163.com>
2020-01-06 21:08:41 +0800

    项目初始化 处理后端返回数据超出js安全整数范围问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,17 @@
 import axios from 'axios'
+import jsonBig from 'json-bigint'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
 })
+
+// 处理大数字类型
+request.defaults.transformRequest = [function (data) {
+  try {
+    return jsonBig.parse(data)
+  } catch (error) {
+
+  }
+}]
 
 export default request
