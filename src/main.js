7ea3c5a0725864c3345668aa37322c18d548ca4f
mxm321 <15863722017@163.com>
2020-01-06 20:44:10 +0800

    项目初始化 样式 less文件结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// vant组件的全局注册
 import './utils/register-vant'
 
+// 加载全局样式
+import './styles/index.less'
 Vue.config.productionTip = false
 
 new Vue({
