af0564cc878dbdae1aae1c75f993a5866ed25e84
mxm321 <15863722017@163.com>
2020-01-09 22:11:29 +0800

    token请求拦截器 响应拦截器 完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import jsonBig from 'json-bigint'
+import store from '@/store'
+import router from '@/router'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
@@ -14,4 +16,45 @@ request.defaults.transformResponse = [function (data) {
   }
 }]
 
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+  const { user } = store.data
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
+// 响应拦截器
+axios.interceptors.response.use(function (response) {
+  return response
+},
+async function (error) {
+  if (error.response && error.response.status === 401) {
+    const user = store.state.user
+    if (!user || !user.refresh_token) {
+      router.push('/login')
+      return
+    }
+    try {
+      const res = await axios({
+        method: 'put',
+        url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${user.refresh_token}`
+        }
+      })
+      store.commit('setUser', {
+        token: res.data.data.token,
+        refresh_token: user.refresh_token
+      })
+      return request(error.config)
+    } catch (error) {
+      router.push('/login')
+    }
+  }
+  return Promise.reject(error)
+})
 export default request
