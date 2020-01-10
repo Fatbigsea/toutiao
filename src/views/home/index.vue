efc289d0aff0f6234a482f7aa3c0fcc981bddaf8
mxm321 <15863722017@163.com>
2020-01-10 19:59:46 +0800

    用户频道列表 完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,19 +2,38 @@
   <div class="home-container">
     <van-nav-bar title="首页" />
     <van-tabs v-model="activeName">
-      <van-tab v-for="index in 8" :key="index" :title="'标签 ' + index">
-        内容 {{ index }}
+      <van-tab v-for="item in channels" :key="item.id" :title="item.name">
+        {{ item.name }}
       </van-tab>
     </van-tabs>
   </div>
 </template>
 
 <script>
+import { getUserChannels } from '@/api/channel'
 export default {
   name: 'HomePage',
   data () {
     return {
-      activeName: 0
+      activeName: 0,
+      channels: []
+    }
+  },
+  methods: {
+    // 获取用户频道列表
+    async getChannel () {
+      try {
+        const { data } = await getUserChannels()
+        this.channels = data.data.channels
+        console.log(this.channels)
+      } catch (error) {
+        this.$toast('用户频道信息获取失败')
+      }
+    }
+  },
+  created () {
+    if (this.$store.state.user) {
+      this.getChannel()
     }
   }
 
