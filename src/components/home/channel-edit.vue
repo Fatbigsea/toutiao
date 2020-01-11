e9609ead3dd88c710c2d19361db62e5c48e40475
mxm321 <15863722017@163.com>
2020-01-11 20:43:14 +0800

    频道组件布局完成

--- src/components/home/channel-edit.vue
+++ src/components/home/channel-edit.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="channel-edit">
-    频道编辑
+    <!-- 我的频道 -->
+    <van-cell-group>
+      <van-cell title="我的频道">
+        <van-button
+          slot="right-icon"
+          type="danger"
+          round
+          size="mini"
+          >
+          编辑
+        </van-button>
+      </van-cell>
+      <van-cell>
+        <van-grid :gutter="10">
+        <van-grid-item
+          v-for="value in 8"
+          :key="value"
+          text="文字"
+        />
+      </van-grid>
+      </van-cell>
+    </van-cell-group>
+
+    <!-- 频道推荐 -->
+    <van-cell-group>
+      <van-cell title="频道推荐">
+      </van-cell>
+      <van-cell>
+        <van-grid :gutter="10">
+        <van-grid-item
+          v-for="value in 8"
+          :key="value"
+          text="文字"
+        />
+      </van-grid>
+      </van-cell>
+    </van-cell-group>
   </div>
 </template>
 
@@ -11,6 +47,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
+  .van-cell-group{
+    margin-top: 50px;
+  }
 
 </style>
